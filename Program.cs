cca39f4e614a3689a66fb5ccdc32f86bdae1013d
Marine_J <marine.julieron@ensc.fr>
2014-11-03 17:23:39 +0100

    First Project Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Mogre;
+
+namespace SMA_Project_V1
+{
+        #region class Program
+        static class Program
+        {
+            [STAThread]
+            static void Main()
+            {
+                try
+                {
+                    BuildingSimulation win = new BuildingSimulation();
+                    win.Go();
+                }
+                catch (System.Runtime.InteropServices.SEHException)
+                {
+                    if (OgreException.IsThrown)
+                        MessageBox.Show(OgreException.LastException.FullDescription, "An Ogre exception has occurred!");
+                    else
+                        throw;
+                }
+            }
+        }
+        #endregion
+ 
+}
