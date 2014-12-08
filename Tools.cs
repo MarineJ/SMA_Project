81240f42fc85df5ae8c4773a0520471f50b59aeb
Marine_J <marine.julieron@ensc.fr>
2014-12-08 16:44:59 +0100

    new file

---
+++ Tools.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SMA_Project_V1
+{
+    static class Tools
+    {
+
+
+    internal static const int LEADERSHIP_UP = 2;
+    internal static const int LEADERSHIP_DOWN = -2;
+    internal static const int ANGRYNESS_UP = 2;
+
+    static internal void updateValue(int diff, int valueToUpdate)
+    {
+        if (valueToUpdate + diff < 0)
+        {
+            valueToUpdate = 0;
+        }
+        else if (valueToUpdate + diff > 100)
+        {
+            valueToUpdate = 100;
+        }
+        else
+        {
+            valueToUpdate += diff;
+        }
+    }
+
+    }
+}
