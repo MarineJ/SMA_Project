406ccea475f50f42834040575ef093a4fe4c55ff
Marine_J <marine.julieron@ensc.fr>
2014-12-08 13:58:40 +0100

    interface

---
+++ IComportement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mogre;
+
+namespace SMA_Project_V1
+{
+    interface IComportement
+    {
+        public abstract bool Comportement(FrameEvent evt, Random rand, Agent agent);
+
+    }
+}
