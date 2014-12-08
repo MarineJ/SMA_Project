1d338e3d1282125dce6e582399aaaad2763c88ac
Marine_J <marine.julieron@ensc.fr>
2014-12-08 14:36:41 +0100

    new try

--- IComportement.cs
+++ IComportement.cs
@@ -9,5 +9,22 @@ namespace SMA_Project_V1
     {
         public abstract bool Comportement(FrameEvent evt, Random rand, Agent agent);
 
+
+        public abstract bool evolve();
+
+        public abstract bool regress();
+
+        public abstract void remaneWhatYouAre();
+
+        public abstract void negociateWithManager();
+
+        public abstract void negociateWithDrag();
+
+        public abstract void negociateWithIdler();
+
+        public abstract void negociateWithBuilder();
+
+
+
     }
 }
